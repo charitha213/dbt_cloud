24ad468a363e40d26279cc12913c794a92ed77fd
charitha sree kankipati <charithasree.kankipati@navikenz.com>
2025-09-11 18:24:43 +0000

    successfully able to add logs into dbt_logs table in dbx

--- models/bronze/bronze_claims_dbt.sql
+++ models/bronze/bronze_claims_dbt.sql
@@ -1,10 +1,11 @@
 {{ config(
     materialized='table',
-    schema='bronze'
+    schema='bronze',
+    post_hook=[
+        "INSERT INTO logs.dbt_logs (dataset, time_processed, source_records, target_records, bad_records, model_name, run_id, status, execution_time_seconds, created_at) SELECT 'bronze_claims_dbt', current_timestamp(), NULL, (SELECT COUNT(*) FROM {{ this }}), 0, 'bronze_claims_dbt', '{{ invocation_id }}', 'SUCCESS', NULL, current_timestamp()"
+    ]
 ) }}
 
 
 SELECT * FROM delta.`/Volumes/insurance/landing/streaming/claims/`
 
--- at the bottom of the file
-{{ log_bronze_run('bronze_claims_dbt') }}
